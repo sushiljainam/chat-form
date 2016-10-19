3c4213e4402c14abab943786f08b302ac232c952
sushiljainam <sushiljain1993@gmail.com>
2016-10-19 12:33:10 +0530

    5

--- index.js
+++ index.js
@@ -3,17 +3,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+    io.emit('chat message', msg);
   });
-  socket.on('disconnect', function(){
-  console.log('user disconnected');
-});
 });
 
 http.listen(3000, function(){
