9af1d77225721a085b32a00b692e6a4310e68e5c
sushiljainam <sushiljain1993@gmail.com>
2016-10-25 14:41:45 +0530

    signup html added

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/signup', function(req, res){
+  res.sendFile(__dirname + '/signup.html');
+});
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
