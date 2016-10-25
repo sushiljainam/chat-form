50e1a3203eac1a257b671a6562f88d7554a3f4d2
sushiljainam <sushiljain1993@gmail.com>
2016-10-25 13:07:04 +0530

    port changed

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+var portNumber = 3003;
+http.listen(portNumber, function(){
+  console.log('listening on localhost:'+portNumber);
 });
